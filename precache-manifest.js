a797c0e1fdd7e1758945fb234a9787bd80ad1309
maxcowalenko <max.cowalenko2012@yandex.ru>
2022-01-11 12:34:46 +0300

    18

--- precache-manifest.js
+++ precache-manifest.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/manifest.json"
   },
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/favicon.png"
   },
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/"
   },
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/index.html"
   },
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/global.css"
   },
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/build/bundle.css"
   },
   {
-    "revision": "17",
+    "revision": "18",
     "url": "/build/bundle.js"
   }
 ]);
